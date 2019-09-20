25335116a102c578a184f42436f6589b07dcf2ed
Jordan <jcrerar@gmail.com>
2019-09-20 17:25:31 -0400

    xllayout added

--- app/src/main/java/com/fluidmatterdevelopment/devprofile/MainActivity.kt
+++ app/src/main/java/com/fluidmatterdevelopment/devprofile/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.fluidmatterdevelopment.devprofile
 
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //For rounded corners on logoImg
+            val logoImgBitmap = RoundedBitmapDrawableFactory.create(
+                resources, BitmapFactory.decodeResource
+                    (resources, R.drawable.devslopesprofilelogo))
+            logoImgBitmap.cornerRadius = 15f
+            logoImg.setImageDrawable(logoImgBitmap)
     }
+
 }
